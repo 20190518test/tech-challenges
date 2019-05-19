b912de64cbf5315a45ab7ac381c5005fea7880d0
Test Tech <test@tech.com>
2019-05-19 17:42:28 +0200

    Backend Test

--- backend/src/Client/Webapp/app.php
+++ backend/src/Client/Webapp/app.php
@@ -7,6 +7,9 @@ if (file_exists(ROOT_PATH.'/vendor/autoload.php') === false) {
 }
 require_once ROOT_PATH.'/vendor/autoload.php';
 
+use IWD\JOBINTERVIEW\Client\Webapp\Data\Builder;
+use IWD\JOBINTERVIEW\Client\Webapp\Survey\SurveyException;
+use IWD\JOBINTERVIEW\Client\Webapp\Data\SurveyResult;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Silex\Application;
@@ -17,6 +20,41 @@ $app->after(function (Request $request, Response $response) {
 });
 $app->get('/', function () use ($app) {
     return 'Status OK';
+});
+
+$app->get('/read', function () use ($app) {
+
+    $files = scandir(Builder::FILES_PATH);
+    $data = Builder::extractData($files);
+
+    $surveyResult = new SurveyResult();
+    $surveys = $surveyResult->getSurveyNames($data);
+
+    return json_encode($surveys);
+});
+
+$app->get('/aggregate/{surveyCode}', function (string $surveyCode) use ($app) {
+
+    try {
+
+        $files = scandir(Builder::FILES_PATH);
+        $data = Builder::extractData($files);
+
+        if (empty($data[$surveyCode])) {
+            throw new SurveyException(sprintf('This survey doesnt exist: %s', $surveyCode));
+        }
+
+        $surveyResults = $data[$surveyCode]['questions'];
+
+        $surveyResult = new SurveyResult();
+        $aggregatedSurvey = $surveyResult->aggregateResults($surveyResults);
+        $output = $surveyResult->output($aggregatedSurvey);
+
+    } catch (\Exception $e) {
+        $output = [$e->getMessage()];
+    }
+
+    return json_encode($output);
 });
 
 $app->run();
